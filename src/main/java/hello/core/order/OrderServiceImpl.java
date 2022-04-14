eb35504c4519fc11e3e9614fc4efaaf49d6bfd07
sungwoo <ssw890418@gmail.com>
2022-04-14 17:21:24 +0900

    chore: dip 위반 사례 수정
    외부에서 구현체를 주입받도록 생성자 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,21 +2,24 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberService memberService = new MemberServiceImpl();
-    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    private DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
 
-        Member member = memberService.findMember(memberId);
+        Member member = memberRepository.findById(memberId);
         int discount = discountPolicy.discount(member, itemPrice);
 
         return new Order(memberId, itemName, itemPrice, discount);
