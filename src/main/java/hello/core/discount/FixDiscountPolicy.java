810e9b47d28578449f75f5938328a0a9037a6c79
sungwoo <ssw890418@gmail.com>
2022-04-14 17:15:44 +0900

    feat: 할인정책 추가

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private final int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int orderPrice) {
+
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }
+
+        return 0;
+    }
+}
